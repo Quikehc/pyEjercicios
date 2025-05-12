240710b49e722ab6d0ec4e974381627d90b4bce1
gabriel <152113631+Quikehc@users.noreply.github.com>
2025-05-12 13:34:18 -0300

    eje11.py

--- eje11.py
+++ eje11.py
@@ -1,11 +1,11 @@
 def eje11():
 
     autos = int(input("Autos vendidos: "))
-    valor = [] * autos
+    valor = []
     salario = 0
 
     for i in range(autos):
-        valor[i] = int(input("valor del auto:"))
+        valor.append(int(input("valor del auto:"))
         salario = salario + valor[i]*0.05 + 200
         print("Salario: ", int(5500 + salario))
 eje11()
